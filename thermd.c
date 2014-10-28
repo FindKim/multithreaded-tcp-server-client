567b2052792f1300bdaa0e3f142c8207326898d6
Dinh Ngoc Do <ddo@student01.cse.nd.edu>
2014-10-28 17:34:02 -0400

    basic outline, has errors when run though.

--- thermd.c
+++ thermd.c
@@ -20,6 +20,199 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
+
+char* GROUPNO = "g05";
+
+
+// Struct for temperature reading
+typedef struct {
+	char hostname[32];	// Name of host with the sensors
+	int nsensor;				// Number of sensors
+	int sensor_num;			// Sensor number
+	double low, high;	// Acceptable low/high value for this sensor
+	double data;				// Sensor reading
+	char timestamp[32];	// Timestamp
+	int action;					// Action requested
+	// action == 0: Send sensor data
+	// action == 1: Request status
+	// Sensor data fields in your struct should be set to zero for a “request status” packet.
+} sensorInfo;
+
+// Function to receive data from client and fill struct
+void * rec_struct (void * param){
+	
+	int r = *((int *) param);
+	sensorInfo *x, *x2;	
+	int rbyte;
+	char *data, *data2, *tok, *tok2;
+	uint16_t datasize;
+	char *logfile;
+
+	//read data size from client
+	if((rbyte = read(r, &datasize, sizeof(uint16_t))) < 0){
+		fprintf(stderr, "Error in datasize read\n");
+		return 0;
+	}
+
+	printf("Datasize = %d", datasize);
+	datasize = ntohs(datasize);
+	
+	//read data from client
+	if((rbyte = read(r, data, datasize)) < 0){
+		fprintf(stderr, "Error in data read\n");
+		return 0;
+	}
+	
+	printf("Read data, now filling 1st struct\n");
+
+	//Parse through data and fill first struct
+	char *structfill = strtok(data, ",");
+	strncpy(x->hostname, structfill, 32);
+	structfill = strtok (NULL, ",");
+	x->nsensor = atoi(structfill);
+	structfill = strtok (NULL, ",");
+	x->sensor_num = atoi(structfill);
+	structfill = strtok(NULL, ",");
+	x->low = strtod(structfill, NULL);
+	structfill = strtok(NULL, ",");
+	x->high = strtod(structfill, NULL);
+	structfill = strtok(NULL, ",");
+	x->data = strtod(structfill, NULL);
+	structfill = strtok(NULL, ",");
+	strncpy(x->timestamp, structfill, 32);
+	structfill = strtok(NULL, ",");
+	x->action = atoi(structfill);
+
+	//If action == 0, append data to logfile
+	/*if(x->action == 0){
+		char *datetok = strtok (x->timestamp, " ");
+		sprintf(logfile, "/var/log/therm/temp_logs/g05_"
+		
+	*/
+	
+
+	if(x->nsensor == 2){
+		
+		//read data size from client
+		if((rbyte = read(r, &datasize, sizeof(uint16_t))) < 0){
+			fprintf(stderr, "Error in datasize read\n");
+			return 0;
+		}
+
+		datasize = ntohs(datasize);
+	
+		//read data from client
+		if((rbyte = read(r, data2, datasize)) < 0){
+			fprintf(stderr, "Error in data read\n");
+			return 0;
+		}
+	
+		printf ("Finished reading data, now filling 2nd struct\n");
+		//Parse through data and fill second struct
+		char *structfill2 = strtok(data2, ",");
+		strncpy(x->hostname, structfill, 32);
+		structfill2 = strtok (NULL, ",");
+		x2->nsensor = atoi(structfill2);
+		structfill2 = strtok (NULL, ",");
+		x2->sensor_num = atoi(structfill2);
+		structfill2 = strtok(NULL, ",");
+		x2->low = strtod(structfill2, NULL);
+		structfill2 = strtok(NULL, ",");
+		x2->high = strtod(structfill2, NULL);
+		structfill2 = strtok(NULL, ",");
+		x2->data = strtod(structfill2, NULL);
+		structfill2 = strtok(NULL, ",");
+		strncpy(x->timestamp, structfill, 32);
+		structfill2 = strtok(NULL, ",");
+		x2->action = atoi(structfill2);
+	}
+
+	printf("FROM SERVER: Hostname: %s\nNumber of Sensors: %d\nSensor Number: %d\nLow: %.1f\nHigh: %.1f\nReading: %.1f\nTimestamp: %s\n", x->hostname, x->nsensor, x->sensor_num, x->low, x->high, x->data, x->timestamp);
+
+	printf("FROM SERVER: Hostname: %s\nNumber of Sensors: %d\nSensor Number: %d\nLow: %.1f\nHigh: %.1f\nReading: %.1f\nTimestamp: %s\n", x2->hostname, x2->nsensor, x2->sensor_num, x2->low, x2->high, x2->data, x2->timestamp);
+
+	close(r);
+}
+		
+	
+
+
+int main (int argc, char *argv[]){
+
+	int r, s;
+	char *portno = "9766";
+	struct addrinfo hint, *sinfo, *t;
+	struct sockaddr_storage servaddr;
+
+	memset (&hint, 0, sizeof(struct addrinfo));
+	hint.ai_family = AF_UNSPEC;
+	hint.ai_socktype = SOCK_STREAM;
+	hint.ai_flags = AI_PASSIVE;
+
+	if((r = getaddrinfo(NULL, portno, &hint, &sinfo)) != 0){
+		perror("Error in getaddrinfo\n");
+		return 1;
+	}
+		
+	for (t = sinfo; t != NULL; t = t->ai_next){
+		if((s = socket(t->ai_family, t->ai_socktype, t->ai_protocol)) == -1){
+			perror("server:scocket error\n");
+			continue;
+		}
+
+		int opt = 1;
+		if(setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(int)) == -1){
+			perror("Setsockopt error\n");
+			exit(1);
+		}
+	
+		if(bind(s, t->ai_addr, t->ai_addrlen) == -1){
+			close (s);
+			perror ("server: bind error\n");
+			continue;
+		}
+
+		break;
+	}
+
+	
+	//Check if bind was successful
+	if (t==NULL){
+		fprintf(stderr, "Error in bind\n");
+		return 1;
+	}
+
+	freeaddrinfo(sinfo);
+
+	//Listen for clients
+	while (listen(s, 5) > -1) {
+
+		socklen_t sinlen = sizeof(struct sockaddr_storage);
+
+		//Create new socket
+		if((r = accept(s, (struct sockaddr *)&servaddr, &sinlen))< 0){		
+			fprintf(stderr, "Error accepting\n");
+			return 1;
+		}
+
+		
+		//Create new thread & run rec_struct function
+		pthread_t id;
+		if(pthread_create(&id, NULL, rec_struct, &r) < 0){
+			fprintf(stderr, "Error creating thread\n");
+			return 1;
+		}
+		
+	}
+
+	
+
+	
+	return 0;
+}
+
+
 
 
 
